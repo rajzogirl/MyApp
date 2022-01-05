ba8d326456dddf6e780b8f2a335e58ae304a392c
rajzogirl <sajka1234@gmail.com>
2022-01-05 21:59:40 +0100

    Implemented Application State

--- app/src/main/java/com/example/myapp/ApplicationState.java
+++ app/src/main/java/com/example/myapp/ApplicationState.java
@@ -1,2 +1,25 @@
-package com.example.myapp;public class ApplicationState {
+package com.example.myapp;
+
+public class ApplicationState {
+        private static ApplicationState instance = null;
+
+        private Lesson lesson = null;
+
+        private ApplicationState() { }
+
+        public static ApplicationState getInstance() {
+            if (instance == null){
+            instance = new ApplicationState();
+        }
+            return instance;
+        }
+
+        public void startNewLesson(){
+            lesson = new Lesson();
+        }
+
+        public Lesson getLesson() {
+            return lesson;
+        }
+
 }
