721c7e4b4c0507c60b06669d9a693b29dff453b1
rajzogirl <sajka1234@gmail.com>
2022-01-06 20:44:33 +0100

    Added picking n random words from given map

--- app/src/main/java/com/example/myapp/Lesson.java
+++ app/src/main/java/com/example/myapp/Lesson.java
@@ -1,24 +1,46 @@
 package com.example.myapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
 
 public class Lesson {
-    HashMap<String, String> translationMap = new HashMap<String, String>() {{
-        put ("good", "dobry");
-        put ("nice", "miły");
 
-    }};
-
-    int wordsCount = 0;
+    int wordsCount;
     int correctAnswerCount = 0;
     int currentWordNumber = 0;
-
     String[] wordsToTranslate;
+    HashMap<String, String> translationMap;
 
-    public Lesson(){
-        wordsToTranslate = translationMap.values().toArray(new String[0]);
-        wordsCount = wordsToTranslate.length;
+    private HashMap<String, String> getNRandomWords(HashMap<String, String> translationMap, int count){
+        HashMap<String, String> hashMap = new HashMap<>();
+        Random random = new Random(System.currentTimeMillis());
+
+        if(count > translationMap.size()){
+            count = translationMap.size();
+        }
+        Map.Entry<String, String>[] array = translationMap.entrySet().toArray(new Map.Entry[0]);
+        List<Map.Entry<String, String>> list = new ArrayList<>(Arrays.asList(array));
+
+        for (int i = count; i > 0; i--){
+            int randomNumber = random.nextInt(count);
+            Map.Entry<String, String> randomEntry = list.get(randomNumber);
+            hashMap.put(randomEntry.getKey(), randomEntry.getValue());
+            list.remove(randomNumber);
+
+        }
+
+        return hashMap;
+    }
+
+    public Lesson(HashMap<String, String> translationMap, int wordsCount){
+        this.translationMap = getNRandomWords(translationMap, wordsCount);
+        wordsToTranslate = this.translationMap.values().toArray(new String[0]);
+        this.wordsCount = wordsToTranslate.length;
     }
 
     public boolean checkWord(String word) {
