9617b05157d2c53b6c188e17e8a46da380f02697
rajzogirl <sajka1234@gmail.com>
2022-01-06 20:45:28 +0100

    Added lesson mode

--- app/src/main/java/com/example/myapp/ApplicationState.java
+++ app/src/main/java/com/example/myapp/ApplicationState.java
@@ -1,9 +1,35 @@
 package com.example.myapp;
 
+import java.util.HashMap;
+
 public class ApplicationState {
         private static ApplicationState instance = null;
 
         private Lesson lesson = null;
+
+        private final HashMap<String, String> polishToEnglishTranslationMap = new HashMap<String, String>(){{
+            put ("good", "dobry");
+            put ("nice", "miły");
+            put ("strong", "silny");
+            put ("dog", "pies");
+            put ("cat", "kot");
+            put ("mirror", "lustro");
+            put ("tree", "drzewo");
+            put ("window", "okno");
+        }};
+
+        private final HashMap<String, String> englishToPolishTranslationMap = new HashMap<String, String>(){{
+            put ("ciekawy", "interesting");
+            put ("dobry", "good");
+        }};
+
+        public enum LessonMode {
+            ENGLISH_TO_POLISH,
+            POLISH_TO_ENGLISH
+        }
+        private LessonMode lessonMode;
+
+
 
         private ApplicationState() { }
 
@@ -14,12 +40,21 @@ public class ApplicationState {
             return instance;
         }
 
-        public void startNewLesson(){
-            lesson = new Lesson();
+        public void startNewLesson(LessonMode lessonMode){
+            this.lessonMode = lessonMode;
+                    if (lessonMode == LessonMode.POLISH_TO_ENGLISH){
+                        lesson = new Lesson(polishToEnglishTranslationMap, 6);
+                    }else if (lessonMode == LessonMode.ENGLISH_TO_POLISH){
+                        lesson = new Lesson(englishToPolishTranslationMap, 6);
+                    }
         }
 
         public Lesson getLesson() {
             return lesson;
+        }
+
+        public LessonMode getLessonMode(){
+            return lessonMode;
         }
 
 }
